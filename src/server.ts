38605bf35e004b51f40b388d0875ccb889bed30b
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-02-02 19:18:41 +0200

    initial setup

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_req, res) => {
+  res.send("Hello homepage");
+});
+
+app.listen(3000, () => console.log(`Server started on port ${3000}`));
